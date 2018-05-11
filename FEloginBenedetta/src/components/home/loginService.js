cb4930ee75fa7250e65aaf36f048bf268a7e3509
benedetta <bene.capponi@gmail.com>
2018-05-11 18:22:53 +0200

    BENEDETTA: tutto ok

--- FEloginBenedetta/src/components/home/loginService.js
+++ FEloginBenedetta/src/components/home/loginService.js
@@ -2,12 +2,23 @@ import axios from "axios";
 
 export default class LoginService{
    login(username, password, onSuccess, onError){
-      axios.post("http://localhost:8070/authentication/logIn", {username:username, password:password}).then(function(result){
-         console.log("Login effettuato con successo, token: ",result.data);
+       console.log(username, password);
+       const data=JSON.stringify({
+        'username' :username, 'password': password
+       });
+
+       const config={
+           headers:{'Content-Type': 'application/jason; charset=UTF-8', 'Access-Control-Allow-Origin': '*',
+           },
+       };
+
+      axios.post("http://localhost:8070/authentication/logIn", {data,},config).then(function(result){
+      console.log(result);   
+      console.log("Login effettuato con successo, token: ",result.data);
          onSuccess(result.data);
       }, function(error){
-         console.error("Errore durante il login: ", error);
-         onError(error.response.data);
+         console.error(error);
+         onError(error.response);
       });
   }
 }
